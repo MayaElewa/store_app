7fc56e1c9646ad3c554d095d81d00a9e1add0e5c
mayaElawa <your@email.com>
2025-08-27 17:13:40 +0300

    feat: Initialize Flutter iOS project with essential files and configurations
    - Added iOS workspace and project files for Flutter integration.
    - Created AppDelegate.swift for Flutter app lifecycle management.
    - Included asset catalog for app icons and launch images.
    - Set up Info.plist with necessary configurations for the app.
    - Implemented basic product model and service for future API integration.
    - Established initial test structure for widget testing.
    - Configured pubspec.yaml with dependencies and project metadata.

---
+++ lib/models/product_model.dart
@@ -0,0 +1,42 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class ProductModel {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  //String category;
+  final String image;
+  final RatingModel rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.image,
+  });
+
+  factory ProductModel.fromJson(jsonData) {
+    return ProductModel(
+        id: jsonData["id"],
+        title: jsonData["title"],
+        price: (jsonData["price"] as num).toDouble(),
+        description: jsonData["description"],
+        image: jsonData["image"],
+        rating: RatingModel.fromjson(jsondata["rating"]));
+  }
+}
+
+class RatingModel {
+  final double rate;
+  final int count;
+
+  RatingModel({required this.rate, required this.count});
+
+  factory RatingModel.fromjson(jsondata) {
+    return RatingModel(rate: jsondata["rate"], count: jsondata["count"]);
+  }
+}
